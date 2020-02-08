50d71291e31fc1370b61c6af852fb1a24c2e2130
Marise St-Pierre <naranjaqc@hotmail.com>
2020-02-07 20:16:56 -0500

    Updates

---
+++ precache-manifest.aa6486fcd835e7df9d49c40bb5960c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb9fd3010bd5ba7e95168df1469b651a",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "15d3fe19574364950900",
+    "url": "/lottery/static/css/main.700ed3ed.chunk.css"
+  },
+  {
+    "revision": "7a502df333910b495f6d",
+    "url": "/lottery/static/js/2.c8a34041.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lottery/static/js/2.c8a34041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d3fe19574364950900",
+    "url": "/lottery/static/js/main.64fd2aa4.chunk.js"
+  },
+  {
+    "revision": "7e340b6feafb810d9c0f",
+    "url": "/lottery/static/js/runtime-main.80f100ae.js"
+  }
+]);
